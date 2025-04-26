bcf6440396619d5df7689a0f33728bc673b53241
Aninanuna <nfominceva1979@gmail.com>
2025-04-26 12:36:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function openModal() {
+  document.getElementById('myModal').style.display = 'flex';
+}
+function closeModal() {
+  document.getElementById('myModal').style.display = 'none';
+}
+window.onclick = function(event) {
+  if (event.target == document.getElementById('myModal')) {
+    closeModal();
+  }
+}
